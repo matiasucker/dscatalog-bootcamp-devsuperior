4ff7cb3413beb70a985c0da5b405ed3d85bce816
matiasucker <matias.ucker@gmail.com>
2021-07-16 16:03:14 -0300

    testdb h2

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,15 +2,24 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	public Category() {
-		
-		
+
 	}
 
 	public Category(Long id, String name) {
@@ -58,8 +67,5 @@ public class Category implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
 
 }

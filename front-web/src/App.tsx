38ec5a848cf3fe59e407ebc3c95add96381799a6
matiasucker <matias.ucker@gmail.com>
2021-07-28 16:17:44 -0300

    add Bootstrap

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Alert from './Alert';
 
 const App = () => {
-    return <h1>Hello world!</h1>
+    return (
+        <div className="container mt-5">
+            <Alert text="Washington" />
+            <Alert text="Nélio" />
+            <Alert />
+            <Alert />
+        </div>
+    );
 }
 
 export default App;

e26e3b8dbb95014779b3f8fcc35c688f8c2d0e66
matiasucker <matias.ucker@gmail.com>
2021-08-29 16:27:15 -0300

    Update readme

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import Alert from './Alert';
+import React, {useState, useEffect} from 'react';
 
 const App = () => {
+    const [counter, setCounter] = useState(0);
+
+useEffect(() => {
+    console.log('o contador mudou de valor')
+}, []);
+
     return (
         <div className="container mt-5">
-            <Alert text="Washington" />
-            <Alert text="Nélio" />
-            <Alert />
-            <Alert />
+            <button 
+                className="btn btn-primary mr-5"
+                onClick={() => setCounter(counter + 1)}
+            >
+                +
+            </button>
+            <span>
+                {counter}
+            </span>
+            <button 
+                className="btn btn-primary ml-5"
+                onClick={() => setCounter(counter - 1)}
+            >
+                -
+            </button>
+            {counter > 5 && <h1>O valor é maior que 5</h1>}
+            {counter <= 5 && <h1>O valor é menor ou igual que 5</h1>}
         </div>
     );
 }

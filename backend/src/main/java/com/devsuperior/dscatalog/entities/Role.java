7d483b99df69b0909e38344003fc91ada6aebd6f
matiasucker <matias.ucker@gmail.com>
2021-09-16 14:08:31 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Role.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Role.java
@@ -2,19 +2,29 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
 public class Role implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String authoriry;
+	private String authority;
 	
 	public Role() {
 	}
 
-	public Role(Long id, String authoriry) {
+	public Role(Long id, String authority) {
 		super();
 		this.id = id;
-		this.authoriry = authoriry;
+		this.authority = authority;
 	}
 
 	public Long getId() {
@@ -25,12 +35,12 @@ public class Role implements Serializable{
 		this.id = id;
 	}
 
-	public String getAuthoriry() {
-		return authoriry;
+	public String getAuthority() {
+		return authority;
 	}
 
-	public void setAuthoriry(String authoriry) {
-		this.authoriry = authoriry;
+	public void setAuthoriry(String authority) {
+		this.authority = authority;
 	}
 
 	@Override

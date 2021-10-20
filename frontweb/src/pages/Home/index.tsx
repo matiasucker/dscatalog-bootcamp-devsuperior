4b2b830be7561e4a32f72415d37f4ed237b1dc44
matiasucker <matias.ucker@gmail.com>
2021-10-20 20:28:43 -0300

    Home styling

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import Navbar from 'components/Navbar';
 
-import './styles.css'
+import './styles.css';
 
 const Home = () => {
   return (
@@ -10,14 +10,19 @@ const Home = () => {
       <Navbar />
       <div className="home-container">
         <div className="home-card">
-            <div className="home-content-container">
-                <h1>Conheça o melhor catálogo de produtos</h1>
-                <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
-                <ButtonIcon />
+          <div className="home-content-container">
+            <div>
+              <h1>Conheça o melhor catálogo de produtos</h1>
+              <p>
+                Ajudaremos você a encontrar os melhores produtos disponíveis no
+                mercado.
+              </p>
             </div>
-            <div className="home-image-container">
-                <MainImage />
-            </div>
+            <ButtonIcon />
+          </div>
+          <div className="home-image-container">
+            <MainImage />
+          </div>
         </div>
       </div>
     </>

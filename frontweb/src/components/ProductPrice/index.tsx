5bdae4d54e3a779a9ca6af661b680956472e6d02
matiasucker <matias.ucker@gmail.com>
2021-10-20 23:11:58 -0300

    creating the ProductPrice component

---
+++ frontweb/src/components/ProductPrice/index.tsx
@@ -0,0 +1,16 @@
+import './styles.css';
+
+const ProductPrice = () => {
+
+    return (
+        <div className="product-price-container">
+            <span>R$</span>
+            <h3>2345.67</h3>
+        </div>
+
+
+    );
+
+}
+
+export default ProductPrice;

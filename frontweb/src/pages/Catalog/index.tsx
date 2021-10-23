ec9b586e49467059bbec6211b2ba8bc9d898bfb9
matiasucker <matias.ucker@gmail.com>
2021-10-23 18:52:53 -0300

    Admin, Navbar HTML

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -40,29 +40,29 @@ const Catalog = () => {
           </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-        <Link to="/products/1">
+          <Link to="/products/1">
             <ProductCard product={product} />
           </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-        <Link to="/products/1">
+          <Link to="/products/1">
             <ProductCard product={product} />
           </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-        <Link to="/products/1">
+          <Link to="/products/1">
             <ProductCard product={product} />
           </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-        <Link to="/products/1">
+          <Link to="/products/1">
             <ProductCard product={product} />
           </Link>
         </div>
       </div>
-    <div className="row">
-      <Pagination />
-    </div>
+      <div className="row">
+        <Pagination />
+      </div>
     </div>
   );
 };

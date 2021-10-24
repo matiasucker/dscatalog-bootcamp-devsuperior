d61c0e75f6119edd4b932f8e7d186366e845b193
matiasucker <matias.ucker@gmail.com>
2021-10-24 01:37:17 -0300

    React Hooks useState and useEffect

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,6 +1,7 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
 import { BASE_URL } from 'util/requests';
@@ -9,14 +10,14 @@ import './styles.css';
 
 const ProductDetails = () => {
 
-  // FORMA INCORRETA
-  let product : Product;
+  const [product, setProduct] = useState<Product>();
 
-  // FORMA INCORRETA
-  axios.get(BASE_URL + "/products/2")
-    .then(response => {
-      console.log(response.data)
+  useEffect(() => {
+    axios.get(BASE_URL + "/products/2")
+      .then(response => {
+        setProduct(response.data);
     });
+  }, []);
 
   return (
     <div className="product-details-container">
@@ -31,23 +32,20 @@ const ProductDetails = () => {
           <div className="col-xl-6">
             <div className="img-container">
               <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
-                alt=""
+                src={product?.imgUrl}
+                alt={product?.name}
               />
             </div>
             <div className="name-price-container">
-              <h1>Nome do produto</h1>
-              <ProductPrice price={2345.67} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do produto</h2>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Accusamus at eveniet obcaecati fuga veritatis rerum iusto sit
-                fugiat, perspiciatis ipsum deleniti laborum illo incidunt
-                laudantium velit quisquam quaerat? Fuga, asperiores.
+                {product?.description}
               </p>
             </div>
           </div>

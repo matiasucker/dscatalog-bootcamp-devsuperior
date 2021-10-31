f68a368de290eb579c7b6a86f3e3c97961b0cde6
matiasucker <matias.ucker@gmail.com>
2021-10-31 17:55:42 -0300

    Making the product paging request

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -2,30 +2,45 @@ import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
 import Pagination from 'components/Pagination';
+import { useState, useEffect } from 'react';
+import { SpringPage } from 'types/vendor/spring';
+import { AxiosParams } from 'types/vendor/axios';
+import { BASE_URL } from 'util/requests';
+import axios from 'axios';
 
 import './styles.css';
 
 const Catalog = () => {
-  const product: Product = {
-    id: 2,
-    name: 'Smart TV',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    price: 2190.0,
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-    date: '2020-07-14T10:00:00Z',
-    categories: [
-      {
-        id: 1,
-        name: 'Livros',
+
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    
+    const params : AxiosParams = {
+      method: 'GET',
+      url: `${BASE_URL}/products`,
+      params: {
+        page: 0,
+        size: 12
       },
-      {
-        id: 3,
-        name: 'Computadores',
-      },
-    ],
-  };
+    }
+
+    axios(params)
+    .then(response => {
+      setPage(response.data);
+    });
+
+  }, []);
+
+  const product =  {
+    "id": 1,
+    "name": "The Lord of the Rings",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "price": 90.5,
+    "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg",
+    "date": "2020-07-13T20:50:07.123450Z",
+    "categories": []
+}
 
   return (
     <div className="container my-4 catalog-container">
---
+++ frontweb/src/types/vendor/axios.ts
@@ -0,0 +1,10 @@
+import { Method } from "axios";
+
+
+
+export type AxiosParams = {
+  method?: Method;
+  url: string;
+  data?: object;
+  params?: object;
+};

c352ee78b4bf620ca0fbeafaf0943d2744bc546c
matiasucker <matias.ucker@gmail.com>
2021-11-02 16:56:41 -0300

    Decoding the JWT Token

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,6 +1,15 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 import history from './history';
+import jwtDecode from 'jwt-decode';
+
+type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+
+type TokenData = {
+  exp: number;
+  user_name: string;
+  authorities: Role[];
+};
 
 type LoginResponse = {
   access_token: string;
@@ -83,3 +92,13 @@ axios.interceptors.response.use(function (response) {
   }
   return Promise.reject(error);
 });
+
+
+export const getTokenData = () : TokenData | undefined => {
+  try {
+  return jwtDecode(getAuthData().access_token) as TokenData;
+  }
+  catch (error) {
+    return undefined;
+  }
+}

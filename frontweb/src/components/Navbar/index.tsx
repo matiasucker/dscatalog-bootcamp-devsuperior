2432dec8615af0caaf15cb2389c2eff1ac6f16ba
matiasucker <matias.ucker@gmail.com>
2021-11-07 12:57:25 -0300

    Organizing utils

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -2,14 +2,11 @@ import './styles.css';
 import 'bootstrap/js/src/collapse.js';
 
 import { Link, NavLink } from 'react-router-dom';
-import {
-  getTokenData,
-  isAuthenticated,
-  removeAuthData,
-} from 'util/requests';
 import React, { useContext, useEffect } from 'react';
 import history from 'util/history';
 import { AuthContext } from 'AuthContext';
+import { getTokenData, isAuthenticated } from 'util/auth';
+import { removeAuthData } from 'util/storage';
 
 const Navbar = () => {
 

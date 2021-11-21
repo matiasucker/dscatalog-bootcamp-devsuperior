ba00b659db8ab11546d44fc12120c5331b79ae4d
matiasucker <matias.ucker@gmail.com>
2021-11-21 14:31:26 -0300

    Styling form margins and containers

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -7,17 +7,27 @@ const Form = () => {
         <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
 
         <form action="">
-          <div className="row">
-            <div className="col-lg-6">
-              <input type="text" className="form-control base-input" />
-              <input type="text" className="form-control base-input" />
-              <input type="text" className="form-control base-input" />
+          <div className="row product-crud-inputs-container">
+            <div className="col-lg-6 product-crud-inputs-left-container">
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div>
+                <input type="text" className="form-control base-input" />
+              </div>
             </div>
             <div className="col-lg-6">
-              <textarea name="" rows={10} className="form-control base-input"></textarea>
+              <textarea
+                name=""
+                rows={10}
+                className="form-control base-input"
+              ></textarea>
             </div>
           </div>
-          <div>
+          <div className="product-crud-buttons-container">
             <button className="btn btn-outline-danger">CANCELAR</button>
             <button className="btn btn-primary">SALVAR</button>
           </div>

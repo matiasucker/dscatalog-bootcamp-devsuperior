23117d41e7e477ec18e5b0e946a7f37de18c067b
matiasucker <matias.ucker@gmail.com>
2021-11-28 18:10:08 -0300

    Component ProductFilter

---
+++ frontweb/src/components/ProductFilter/index.tsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+const ProductFilter = () => {
+
+    return (
+        <div className="base-card product-filter-container">Search bar</div>
+    );
+};
+
+export default ProductFilter;
